ae46cda91813bc4d15b4e4bcaf473641725ab338
DacPhu <87571273+DacPhu@users.noreply.github.com>
2024-07-22 23:29:16 +0700

    edit form

--- front-end/src/pages/Encrypt/_components/EncryptForm.tsx
+++ front-end/src/pages/Encrypt/_components/EncryptForm.tsx
@@ -69,121 +69,125 @@ const EncryptForm = () => {
   };
 
   return (
-    <div className="row">
-      <div className="col-md-8">
-        <div className="card m-3 p-3">
-          <div className="form-group">
-            <label className="pb-2 ps-1">Text</label>
-            <textarea
-              className="form-control"
-              rows={5}
-              placeholder="Paste your text here..."
-              value={text}
-              onChange={handleTextChange}
-            />
-            <div className="d-flex justify-content-between align-items-center mt-3">
-              <small className="text-muted">0/5,000 characters</small>
-              <div className="form-group">
-                <label className="btn btn-outline-secondary">
-                  Upload file{" "}
-                  <input type="file" hidden onChange={handleFileChange} />
-                </label>
-                {file && <small className="text-muted">{file.name}</small>}
+    <>
+      <div className="row">
+        <div className="col-md-8">
+          <div className="card m-3 p-3">
+            <div className="form-group">
+              <label className="pb-2 ps-1">Text</label>
+              <textarea
+                className="form-control"
+                rows={5}
+                placeholder="Paste your text here..."
+                value={text}
+                onChange={handleTextChange}
+              />
+              <div className="d-flex justify-content-between align-items-center mt-3">
+                <small className="text-muted">0/5,000 characters</small>
+                <div className="form-group">
+                  <label className="btn btn-outline-secondary">
+                    Upload file{" "}
+                    <input type="file" hidden onChange={handleFileChange} />
+                  </label>
+                  {file && <small className="text-muted">{file.name}</small>}
+                </div>
               </div>
             </div>
+            <div className="d-flex justify-content-center align-items-center mt-3">
+              <button onClick={handleSubmit} className="btn btn-dark">
+                Encrypt
+              </button>
+            </div>
           </div>
-          <div className="d-flex justify-content-center align-items-center mt-3">
-            <button onClick={handleSubmit} className="btn btn-dark">
-              Encrypt
-            </button>
+        </div>
+        <div className="col-md-4">
+          <div className="card m-3 p-3">
+            <div className="form-group">
+              <label>Gamma</label>
+              <input
+                type="range"
+                className="form-range"
+                min="0"
+                max="100"
+                value={gamma}
+                onChange={handleGammaChange}
+              />
+            </div>
+            <div className="text-center mt-2">{gamma}</div>
+            <div className="form-group mt-3">
+              <label>Message Base: </label>
+              <select
+                className="ms-2"
+                value={messageBase}
+                onChange={handleMessageBaseChange}
+              >
+                <option value="2">2</option>
+                <option value="4">4</option>
+                <option value="8">8</option>
+                <option value="16">16</option>
+                <option value="32">32</option>
+                <option value="64">64</option>
+                <option value="128">128</option>
+                <option value="256">256</option>
+              </select>
+            </div>
+
+            <div className="form-group mt-3 mb-3">
+              <label>Seed Scheme: </label>
+              <select
+                className="ms-2"
+                value={seedScheme}
+                onChange={handleSeedSchemeChange}
+              >
+                <option value="sha_left_hash">SHA Left Hash</option>
+              </select>
+            </div>
+
+            <div className="form-group">
+              <label>Window Length</label>
+              <input
+                type="number"
+                className="form-control"
+                min={1}
+                value={windowLength}
+                onChange={handleWindowLengthChange}
+              />
+            </div>
+            <div className="form-group">
+              <label>Max New Token Ratio</label>
+              <input
+                type="number"
+                className="form-control"
+                value={maxNewTokenRatio}
+                onChange={handleMaxNewTokenRatioChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Num Beams</label>
+              <input
+                type="number"
+                className="form-control"
+                value={numBeams}
+                onChange={handleNumBeamsChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Repetition Penalty</label>
+              <input
+                type="number"
+                className="form-control"
+                value={repetitionPenalty}
+                onChange={handleRepetitionPenaltyChange}
+              />
+            </div>
           </div>
         </div>
       </div>
-      <div className="col-md-4">
-        <div className="card m-3 p-3">
-          <div className="form-group">
-            <label>Gamma</label>
-            <input
-              type="range"
-              className="form-range"
-              min="0"
-              max="100"
-              value={gamma}
-              onChange={handleGammaChange}
-            />
-          </div>
-          <div className="text-center mt-2">{gamma}</div>
-          <div className="form-group mt-3">
-            <label>Message Base: </label>
-            <select
-              className="ms-2"
-              value={messageBase}
-              onChange={handleMessageBaseChange}
-            >
-              <option value="2">2</option>
-              <option value="4">4</option>
-              <option value="8">8</option>
-              <option value="16">16</option>
-              <option value="32">32</option>
-              <option value="64">64</option>
-              <option value="128">128</option>
-              <option value="256">256</option>
-            </select>
-          </div>
 
-          <div className="form-group mt-3 mb-3">
-            <label>Seed Scheme: </label>
-            <select
-              className="ms-2"
-              value={seedScheme}
-              onChange={handleSeedSchemeChange}
-            >
-              <option value="sha_left_hash">SHA Left Hash</option>
-            </select>
-          </div>
-
-          <div className="form-group">
-            <label>Window Length</label>
-            <input
-              type="number"
-              className="form-control"
-              min={1}
-              value={windowLength}
-              onChange={handleWindowLengthChange}
-            />
-          </div>
-          <div className="form-group">
-            <label>Max New Token Ratio</label>
-            <input
-              type="number"
-              className="form-control"
-              value={maxNewTokenRatio}
-              onChange={handleMaxNewTokenRatioChange}
-            />
-          </div>
-
-          <div className="form-group">
-            <label>Num Beams</label>
-            <input
-              type="number"
-              className="form-control"
-              value={numBeams}
-              onChange={handleNumBeamsChange}
-            />
-          </div>
-
-          <div className="form-group">
-            <label>Repetition Penalty</label>
-            <input
-              type="number"
-              className="form-control"
-              value={repetitionPenalty}
-              onChange={handleRepetitionPenaltyChange}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
+      <div className="hidden">Result</div>
+    </>
   );
 };
 
